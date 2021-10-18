f66e0b1d324396464967d85915509e8fed88b3ab
pavani-weerakoon <66486141+pavani-weerakoon@users.noreply.github.com>
2021-10-18 16:02:40 +0530

    changed users models.py

--- apps/jobs/models.py
+++ apps/jobs/models.py
@@ -7,14 +7,7 @@ from apps.users.models import User, Company, Interviewer, Admin, Candidate
 
 
 class User(AbstractUser):
-    CANDIDATE = 'CANDIDATE',
-    INTERVIEWER = 'INTERVIEW',
-    ADMIN = 'ADMIN',
-    USER_TYPE = [
-        (CANDIDATE, 'Candidate'),
-        (INTERVIEWER, 'Interviewer'),
-        (ADMIN, 'Admin'),
-    ]
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(
         max_length=200,
@@ -28,14 +21,6 @@ class User(AbstractUser):
     )
     contact_number = models.IntegerField(
         null=True
-    )
-    company = models.ForeignKey(
-        Company, related_name='company_user',
-        on_delete=models.CASCADE,
-    )
-    user_type = models.CharField(
-        max_length=100,
-        choices=USER_TYPE,
     )
 
 
--- apps/users/models.py
+++ apps/users/models.py
@@ -21,29 +21,33 @@ class Roles(models.TextChoices):
 class Company(SafeDeleteModel):
     name = models.BinaryField(max_length=100, null=True, blank=True)
     logo = models.TextField(null=True, blank=True)
-    owner = models.ForeignKey('users.User', on_delete=models.CASCADE,
-                              related_name='managing_company', null=True, blank=True)
+    owner = models.OneToOneField('users.User', on_delete=models.CASCADE,
+                                 related_name='managing_company', null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=200,)
+    company_email = models.EmailField(max_length=200, blank=True, null=True)
 
 
 class User(AbstractUser, SafeDeleteModel):
+    CANDIDATE = 'CANDIDATE',
+    INTERVIEWER = 'INTERVIEW',
+    ADMIN = 'ADMIN',
+    USER_TYPE = [
+        (CANDIDATE, 'Candidate'),
+        (INTERVIEWER, 'Interviewer'),
+        (ADMIN, 'Admin'),
+    ]
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    role = models.CharField(
-        max_length=20,
-        choices=Roles.choices,
-        default=Roles.USER
-    )
     phone = models.CharField(max_length=15, null=True, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
-
-    def is_super_admin(self):
-        return self.role == Roles.SUPER_ADMIN or self.is_superuser
-
-    def is_admin_user(self):
-        return self.role in [Roles.ADMIN, Roles.SUPER_ADMIN]
-
-    def is_user(self):
-        return self.role == Roles.USER
+    user_type = models.CharField(
+        max_length=50,
+        choices=USER_TYPE,
+    )
+    company = models.ForeignKey(
+        Company, related_name='company_user',
+        on_delete=models.CASCADE,
+    )
 
     def generate_email_verification_code(self):
         verification = self.email_verifications.create(code=generate_token(6))
@@ -67,25 +71,6 @@ class UserEmailVerification(models.Model):
 
     class Meta:
         ordering = ['-created_at']
-
-
-class Company(models.Model):
-    company_name = models.CharField(
-        max_length=200,
-    )
-    location = models.CharField(
-        max_length=200,
-    )
-    company_email = models.EmailField(
-        max_length=200,
-        blank=True, null=True
-    )
-    owner = models.OneToOneField(
-        "users.User", related_name='managing_company',
-        on_delete=models.CASCADE,
-        max_length=200,
-
-    )
 
 
 class Interviewer(models.Model):
